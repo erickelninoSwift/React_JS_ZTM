886d5de30bed514022627ec1f798f262f54b859d
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-15 05:14:30 +0200

    update data with monsters API

--- 01.monster_rolodex/src/App.js
+++ 01.monster_rolodex/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const usersURL = "https://jsonplaceholder.typicode.com/users";
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  render() {
+    const allusers = async () => {
+      const data = await fetch(usersURL);
+      const response = await data.json();
+
+      this.setState({
+        monsters: response,
+      });
+    };
+
+    allusers();
+
+    return (
+      <div className="App">
+        {this.state.monsters.length > 0 ? (
+          this.state.monsters.map((data) => {
+            const { id, name } = data;
+            return <h1 key={id}>{name}</h1>;
+          })
+        ) : (
+          <h1>Your monsters Array is Empty</h1>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
