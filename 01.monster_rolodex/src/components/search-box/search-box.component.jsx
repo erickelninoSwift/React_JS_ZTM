bcd73d83564dfb364e20c74315d424a3fce543a6
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-19 15:19:28 +0200

    update

--- 01.monster_rolodex/src/components/search-box/search-box.component.jsx
+++ 01.monster_rolodex/src/components/search-box/search-box.component.jsx
@@ -1,17 +1,14 @@
 import { Component } from "react";
 import "./search-box.styles.css";
-class SearchBox extends Component {
-  render() {
-    const { SearchString, nameClass } = this.props;
-    return (
-      <input
-        type="text"
-        className={`search-box ${nameClass}`}
-        placeholder="search monster"
-        onChange={(e) => SearchString(e.target.value)}
-      />
-    );
-  }
-}
+const SearchBox = ({ SearchString, nameClass }) => {
+  return (
+    <input
+      type="text"
+      className={`search-box ${nameClass}`}
+      placeholder="search monster"
+      onChange={(e) => SearchString(e.target.value)}
+    />
+  );
+};
 
 export default SearchBox;
