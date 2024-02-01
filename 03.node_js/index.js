603061f61ee3e3dbff438a29d41e541b4049f22f
Erick El nino <erick.tshimbombo@yahoo.com>
2024-02-01 02:23:04 +0200

    update

--- 03.node_js/index.js
+++ 03.node_js/index.js
@@ -1,15 +1,31 @@
-import http from "http";
-const PORT = 3002;
-const server = http.createServer((request, response) => {
-  console.log(request.method);
-  console.log(request.headers.host);
-  console.log(request.url);
+import express from "express";
+// import http from "http";
+// const PORT = 3002;
+// const server = http.createServer((request, response) => {
+//   console.log(request.method);
+//   console.log(request.headers.host);
+//   console.log(request.url);
+//   const user = {
+//     name: "Erick",
+//     surname: "Jackpot",
+//   };
+//   response.setHeader("content-type", "application/json");
+//   response.end(`${JSON.stringify(user)}`);
+// });
+
+// server.listen(PORT);
+
+const app = express();
+
+app.get("/", (request, response) => {
+  const { headers, method, url } = request;
+
   const user = {
     name: "Erick",
-    surname: "Jackpot",
+    hobby: "Play video games",
   };
   response.setHeader("content-type", "application/json");
-  response.end(`${JSON.stringify(user)}`);
+  response.send(user);
 });
 
-server.listen(PORT);
+app.listen(3002);
