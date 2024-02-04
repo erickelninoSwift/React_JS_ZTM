92465ed94e36cfa5f8665d76222d6a73b3938df0
Erick El nino <erick.tshimbombo@yahoo.com>
2024-02-04 16:08:43 +0200

    update our app with some backend

--- 03.node_js/server.js
+++ 03.node_js/server.js
@@ -1,4 +1,14 @@
-import express from "express";
+import express, { response } from "express";
 import bodyParser from "body-parser";
 
 const PORT = 3005;
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use((request, response, next) => {
+  next();
+});
+
+app.listen(PORT);
