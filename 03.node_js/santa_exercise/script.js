c70c4e97b0d4f738d38fc4fa892d181ed3d3a17e
Erick El nino <erick.tshimbombo@yahoo.com>
2024-02-05 02:59:36 +0200

    managed to fix some node js bugs

--- 03.node_js/santa_exercise/script.js
+++ 03.node_js/santa_exercise/script.js
@@ -1,13 +1,45 @@
 // 1- What floor does santa endup on
 import fs from "fs";
 
-function readFiles() {
+function Question1() {
   fs.readFile("./puzzle.txt", (error, data) => {
     if (error) {
       console.log(`Error was found ${error.message}`);
     }
-    console.log(data.toString());
+    const directions = data.toString();
+    const directionArrays = directions.split("");
+    const answer = directionArrays.reduce((acc, currentData) => {
+      if (currentData === "(") {
+        return (acc += 1);
+      } else if (currentData === ")") {
+        return (acc -= 1);
+      }
+    }, 0);
+
+    console.log(`Santa will stop at floor : ${answer}`);
   });
 }
 
-readFiles();
+function Question2() {
+  fs.readFile("./puzzle.txt", (error, data) => {
+    if (error) {
+      console.log(`Error was found ${error.message}`);
+    }
+    const directions = data.toString();
+    const directionArrays = directions.split("");
+    let accumulator = 0;
+    const answer = directionArrays.some((currentValue) => {
+      if (currentValue === "(") {
+        return (accumulator += 1);
+      } else if (currentValue === ")") {
+        return (accumulator -= 1);
+      }
+      return accumulator < 0;
+    });
+    console.log("Question 2 :", answer);
+  });
+}
+
+// Question1();
+console.log("==============================");
+Question2();
